9557b56a595c73008ed0134548f50beaff54627f
maemo <maemo@maemo-TomXue>
2011-12-06 15:08:48 +0000

    hotplug detection, tested on Ubuntu, to use netlink mechanism

---
+++ hotplug.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/un.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <linux/types.h>
+#include <linux/netlink.h>
+#include <errno.h>
+
+static int init_hotplug_sock(void)
+{
+    struct sockaddr_nl snl;
+    const int buffersize = 16 * 1024 * 1024;
+    int retval;
+    
+    memset(&snl, 0x00, sizeof(struct sockaddr_nl));
+    
+    snl.nl_family = AF_NETLINK;
+    snl.nl_pid = getpid();
+    snl.nl_groups = 1;
+    
+    int hotplug_sock = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
+    if (hotplug_sock == -1) {
+        printf("error getting socket: %s", strerror(errno));
+        return -1;
+    }
+    
+    printf("test errno: %s\n", strerror(errno));
+    
+    /* set receive buffersize */
+    setsockopt(hotplug_sock, SOL_SOCKET, SO_RCVBUFFORCE, &buffersize, sizeof(buffersize));
+    
+    retval = bind(hotplug_sock, (struct sockaddr *) &snl, sizeof(struct sockaddr_nl));
+    
+    if (retval < 0) {
+        printf("bind failed: %s", strerror(errno));
+        close(hotplug_sock);
+        hotplug_sock = -1;
+        return -1;
+    }
+    
+    return hotplug_sock;
+}
+
+#define UEVENT_BUFFER_SIZE      2048
+
+int main(int argc, char* argv[])
+{
+    int hotplug_sock       = init_hotplug_sock();
+    while(1)
+    {
+        char buf[UEVENT_BUFFER_SIZE*2] = {0};
+        recv(hotplug_sock, &buf, sizeof(buf), 0); 
+        printf("%s\n", buf);
+    }
+    
+    return 0;
+}
